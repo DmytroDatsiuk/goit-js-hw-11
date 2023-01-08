fdf08c281506e885a78879927434826363645a38
DDimbe <109931951+DmytroDatsiuk@users.noreply.github.com>
2023-01-08 13:16:34 +0200

    button

--- src/indexs.js
+++ src/indexs.js
@@ -68,7 +68,10 @@ function onLoadMore(e) {
       });
     })
     .catch(error => {
+      // loadMoreBtn.disable()
+      loadMoreBtn.hide()
       console.log(error);
+
     });
 }
 
--- src/new-service.js
+++ src/new-service.js
@@ -1,70 +1,41 @@
-import axios from 'axios';
+import axios, { all } from 'axios';
+import Notiflix from 'notiflix';
 
 export default class NewsApiService {
   constructor() {
     this.url = 'https://pixabay.com/api/';
     this.searchQuery = '';
     this.page = 1;
+    this.per_page = 40;
   }
 
   async fetchArticles() {
-    // console.log(this);
-    // const url = `https://pixabay.com/api/?key=32602095-27dbade4d0732e174c3b141f5&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
-
-    // return fetch(url)
-    //   .then(r => r.json())
-    //   .then(data => {
-    //     // console.log(data);
-    //     this.incrementPage();
-    //     return data;
-    //   });
-
     const params = {
       key: '32602095-27dbade4d0732e174c3b141f5',
       q: this.searchQuery,
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
-      per_page: 40,
+      per_page: this.per_page,
       page: this.page,
     };
 
     const response = await axios.get(this.url, { params }).then(response => {
+      const allPages = Math.ceil(response.data.totalHits / this.per_page);
+
+      if (this.page > allPages) {
+        Notiflix.Notify.failure(
+          "We're sorry, but you've reached the end of search results."
+        );
+
+        return;
+      }
       this.incrementPage();
+
       return response.data;
     });
 
     return response;
-
-    // const response = await axios.get(url, {params})
-    // const incrementPage = await response.this.incrementPage();
-    // const respons = await response.data
-    // return respons
-
-    //* const fetchUsers = async () => {
-    //*   const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    //*   const users = await response.json();
-    //*   return users;
-    //* };
-
-    //* fetchUsers().then(users => console.log(users));
-
-    // const url = 'https://pixabay.com/api/';
-
-    // const params = {
-    //   key: '32602095-27dbade4d0732e174c3b141f5',
-    //   q: this.searchQuery,
-    //   image_type: 'photo',
-    //   orientation: 'horizontal',
-    //   safesearch: true,
-    //   per_page: 40,
-    //   page: this.page,
-    // };
-    // return axios.get(this.url, {params}).then(respons => {
-    //   console.log(respons.data);
-    //   this.incrementPage();
-    //   return respons.data.hits;
-    // });
   }
   incrementPage() {
     this.page += 1;
@@ -73,6 +44,9 @@ export default class NewsApiService {
   resetPage() {
     this.page = 1;
   }
+  // notMorePage() {
+  //   const lastPage =
+  // }
   get query() {
     this.searchQuery;
   }
